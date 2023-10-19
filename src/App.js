c679de25f55591a0cfd51cfdcef6d303253c1993
TharunNB <tharunbhoopathy@gmail.com>
2023-10-19 17:55:15 +0530

    Completed the Project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
+import { marked } from 'marked';
 
+//a heading element (H1 size), 
+//a sub heading element (H2 size), a link, 
+//inline code, a code block, 
+//a list item, a blockquote, an image, 
+//and bolded text.
 function App() {
+  const [textval, settextval] = useState(`
+  # H1
+  ## H2 
+  [Github] (https://github.com/TharunNB)
+  \`code\`
+  \`\`\`
+  {
+    "firstName" : "Tharun",
+    "lastName" : "NB"
+  }
+  \`\`\`
+  - first
+  - second
+  - third
+  ![alt text](image.jpg)
+  >hi
+  **hi**
+  
+  `) ;
+
+  const handletextcchange = event => {
+    settextval(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className='float-child'><textarea 
+        id='editor'
+        value={textval}
+        onChange={handletextcchange}/>
+      </div>
+      <div className='float-child'>
+        <div id='preview' 
+        dangerouslySetInnerHTML={{
+          __html: marked.parse(textval),
+          }}></div>
+      </div>
       </header>
     </div>
   );
